79c3789d0da43cf74e970cedad798cc3e86132c9
yagniktrivedi <yagnikvyagnik@gmail.com>
2024-02-04 15:10:57 +0530

    added video background

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -7,13 +7,13 @@ const VideoBackground = ({ movieId }) => {
   useMovieTrailer(movieId);
 
   return (
-    <div>
+    <div className="w-screen">
       <iframe
-        width="560"
-        height="315"
+        className="w-screen aspect-video"
         src={
           "https://www.youtube.com/embed/UGc5Tzz19UY?si=BsY9mARzeI8PcZn8" +
-          trailerVideo?.key
+          trailerVideo?.key +
+          "&autoplay=1&mute=1"
         }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
