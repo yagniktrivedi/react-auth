f38498a101365fa2a8b6f64756a80aeb9f1a5bdf
Yagnik Trivedi <yagnik.trivedi-c@therapybrands.com>
2024-02-05 18:31:57 +0530

    gpt movie

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import lang from '../utils/languageConstants';
 import { useSelector } from 'react-redux';
+import openai from '../utils/openai';
 
 const GptSearchBar = () => {
+	const searchRef = useRef(null);
 	const langKey = useSelector((state) => state.config.lang);
+
+	const handleGptSearchClick = async () => {
+		console.log('handleGptSearchClick', searchRef.current.value);
+
+		const gptQuery =
+			'Act as movie recommendatnion and suggest movie for query:' +
+			searchRef.current.value +
+			'only give me name of five movies, comma seperated like Gaddar, Sholay';
+		const getResults = await openai.chat.completions.create({
+			messages: [{ role: 'user', content: gptQuery }],
+			model: 'gpt-3.5-turbo',
+		});
+
+		console.log('getResults', getResults);
+		const movieResults = getResults.choices[0].message.content.split(',');
+		console.log('movieResults', movieResults);
+	};
 	return (
 		<div className="pt-[5%] flex justify-center">
-			<form className="w-1/2 bg-black grid grid-cols-12 rounded-xl">
+			<form
+				className="w-1/2 bg-black grid grid-cols-12 rounded-xl"
+				onSubmit={(e) => e.preventDefault()}
+			>
 				<input
+					ref={searchRef}
 					type="text"
 					className="p-4 m-4 col-span-9"
 					placeholder={lang[langKey].getSearchPlaceholder}
 				/>
-				<button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">
+				<button
+					className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg"
+					onClick={handleGptSearchClick}
+				>
 					{lang[langKey].search}
 				</button>
 			</form>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,3 +23,5 @@ export const SUPPORTED_LANGUAGES = [
 	{ identifier: 'hindi', name: 'Hindi' },
 	{ identifier: 'spanish', name: 'Spanish' },
 ];
+
+export const OPENAI_KEY = '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';
